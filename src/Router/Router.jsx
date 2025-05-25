457c22ceb075a75b680b9659afbfc95966ad5e6e
arafat22184 <123alarafat@gmail.com>
2025-05-26 03:13:33 +0600

    added navbar footer and register page

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,10 +1,22 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../Layouts/RootLayout";
+import Home from "../Pages/Home/Home";
+import Register from "../Pages/Register/Register";
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "/register",
+        Component: Register,
+      },
+    ],
   },
 ]);
 
